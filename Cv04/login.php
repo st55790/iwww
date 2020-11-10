f5530207ce127054a4b8d5ed7a969a4917a0a9d1
Tomas Prudky <64365934+st55790@users.noreply.github.com>
2020-11-10 13:01:40 +0100

    add gitignore

---
+++ Cv04/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Správa uživatelů - CRUD</title>
+</head>
+<body>
+<h1>Hello HTML</h1>
+<?php
+    echo "<h1>Hello HTLM from PHP</h1>";
+    echo"Hello PHP";
+?>
+
+</body>
+</html>
